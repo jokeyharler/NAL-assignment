530683937463eca87d3d1495348226a44d2e887e
PhamAnhTu123 <92416931+PhamAnhTu123@users.noreply.github.com>
2023-03-09 19:55:21 +0700

    Add mongoose package

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
 import app from './app';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://127.0.0.1:27017/Events');
+
+const connection = mongoose.connection;
+connection.once("open", () => console.log("mongodb connect succeed"));
 
 const PORT = 5000;
 
